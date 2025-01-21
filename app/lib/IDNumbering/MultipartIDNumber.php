93a6b76c128ebcff446ee07fa578d0ab14fa18cf
Páramo Software <contato@paramosofware.com.br>
2025-01-21 14:04:18 +0000

    FOBDSOP-126 Permitir (provisoriamente) que idnos do tipo serial possuem caracteres não numéricos

--- app/lib/IDNumbering/MultipartIDNumber.php
+++ app/lib/IDNumbering/MultipartIDNumber.php
@@ -157,8 +157,8 @@ class MultipartIDNumber extends IDNumber {
 		if ($separator && $this->formatHas('PARENT', 0)) {
 			// starts with PARENT element so explode in reverse since parent value may include separators
 			$v_proc = preg_replace("!^".preg_quote($this->getParentValue(), '!')."!", "_PARENT_", $value);
-
-      $element_vals = explode($separator, $v_proc);
+	
+			$element_vals = explode($separator, $v_proc);
 
 			$i = 0;
 			foreach ($this->getElements() as $element_info) {
