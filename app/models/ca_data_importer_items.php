8da1ccc84489bd2599fdee54b4c0560c1e666130
Seth <seth@whirl-i-gig.com>
2025-09-04 18:04:46 +0000

    Parse date suffixes

--- app/models/ca_data_importer_items.php
+++ app/models/ca_data_importer_items.php
@@ -801,16 +801,22 @@ class ca_data_importer_items extends BaseModel {
 			'default' => false,
 			'label' => _t('Use mapped value as identifier for purposed of matching existing records via an existing record policy.'),
 		);
-		$settings['allowIdnoReplacement'] = array(
+		$settings['doNotParse'] = array(
 			'formatType' => FT_TEXT,
 			'displayType' => DT_FIELD,
 			'width' => 40, 'height' => 2,
 			'takesLocale' => false,
-			'default' => 0,
-			'label' => _t('Allow replacement of existing idno values'),
-			'description' => _t('When set mappings to idno on existing records with idno values are applied. The default is to not apply idno mappings to existing records when an idno values already exists.')
+			'default' => false,
+			'label' => _t('Do not parse entity values for entity preferred labels.'),
 		);
-		
+		$settings['parseDateSuffix'] = array(
+			'formatType' => FT_TEXT,
+			'displayType' => DT_FIELD,
+			'width' => 40, 'height' => 2,
+			'takesLocale' => false,
+			'default' => false,
+			'label' => _t('Parse trailing dates in entity preferred label values and make available as synthetic __entity_label_date__ mapping value.'),
+		);
 		
 		$this->setAvailableSettings($settings);
 	}
